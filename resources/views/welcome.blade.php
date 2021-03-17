2107acd9a14b1804e5c19c591c0128b517365270
Kavya-uday123 <mca522@rajagiri.edu>
2021-03-17 12:00:44 +0530

    Student

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+<br>
+    <div class="container">
+    <div class="row">
+    <div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+    <div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+   
+    <h5><center>REGISTRATION</center></h5>
+    <br>
+    <table class="table">
+    <tr>
+        <td>Name</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Roll No</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>Adm No</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td>College</td>
+        <td><input type="text" class="form-control"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td><button class="btn btn-primary">REGISTER</button></td>
+    </tr>
+    </table>
+    </div>
+    <div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3 col-xxl-3"></div>
+    </div>
+    </div>
+</body>
+</html>
